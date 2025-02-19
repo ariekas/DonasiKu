2edfa7d0b652e8acadc1bb36c1d1b24b14832427
ariekas <ariariekas@gmail.com>
2025-02-19 22:33:18 +0700

    create: migration dan model doumentasi donasi

--- app/Models/DocumentationDonation.php
+++ app/Models/DocumentationDonation.php
@@ -7,5 +7,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class DocumentationDonation extends Model
 {
-   
+    use HasFactory;
+
+    protected $fillable = [
+        'donation_id',
+        'judul',
+        'deskripsi',
+        'image',
+        'jumlah_donasi',
+    ];
+
+    public function donation()
+    {
+        return $this->belongsTo(Donation::class);
+    }
 }
